c637deba23ce6e27cc13eb0cb7e72d885f1a23f3
Hannah Thorpe <hthorpe@protonmail.com>
2020-05-21 13:49:05 -0400

    day 3 complete

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -50,26 +50,26 @@ class LearningRoute extends Component {
       <div className="learn">
         <Header />
         <section>
-          <h3>What does this mean:</h3>
+          <h2>Translate the word:</h2>
           <span>{nextWord}</span>
           <p>Your total score is: {totalScore}</p>
           <form onSubmit={(e) => this.handleGuess(e)}>
             <label htmlFor="learn-guess-input" id="input">
-              your answer:{" "}
+              What's the translation for this word?
             </label>
             <input
               type="text"
-              id="guess-input"
+              id="learn-guess-input"
               name="guess-input"
               required
               onChange={this.setGuess}
             ></input>
-            <button type="submit">Submit</button>
+            <button type="submit">Submit your answer</button>
           </form>
         </section>
 
-        <p>you got this word correct {wordCorrectCount} times.</p>
-        <p>you got this word incorrect {wordIncorrectCount} times.</p>
+        <main>You have answered this word correctly {wordCorrectCount} times.</main>
+        <main>You have answered this word incorrectly {wordIncorrectCount} times.</main>
       </div>
     );
   }
